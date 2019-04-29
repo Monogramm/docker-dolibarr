2b7cb34a516d4b458855d1a8874c63770b365cd5
mathieu.brunot <mb.mathieu.brunot@gmail.com>
2019-04-29 21:41:26 +0200

    :bug: Check htdocs symlink on every run

--- images/7.0/php7.1-apache/entrypoint.sh
+++ images/7.0/php7.1-apache/entrypoint.sh
@@ -220,11 +220,13 @@ if version_greater "$image_version" "$installed_version"; then
 \$force_install_module = '${DOLI_MODULES}';
 EOF
 
-		# Add a symlink to /var/www/htdocs
-		ln -s /var/www/html /var/www/htdocs
-
 		echo "You shall complete Dolibarr install manually at '${DOLI_URL_ROOT}/install'"
 	fi
+fi
+
+if [ ! -d /var/www/htdocs ]; then
+	# Add a symlink to /var/www/htdocs
+	ln -s /var/www/html /var/www/htdocs
 fi
 
 if [ ! -d /var/www/scripts ]; then
