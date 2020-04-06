0dd77e2dacaa4ad812f3bd1f0896cbee8cd47fe4
mathieu.brunot <mathieu.brunot@monogramm.io>
2020-04-06 04:03:37 +0200

    ✅ Env var for curl tests
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- images/7.0/php7.2-apache-amd64/test/docker_test.sh
+++ images/7.0/php7.2-apache-amd64/test/docker_test.sh
@@ -29,7 +29,7 @@ fi
 # Add your own tests
 # https://docs.docker.com/docker-hub/builds/automated-testing/
 #log "Check Dolibarr app home page..."
-#wget http://dolibarr:80
+#wget http://${DOLI_TEST_HOSTNAME}:80
 
 if [ ! -f /srv/dolibarr/documents/install.lock ]; then
     log 'Installing Dolibarr...'
@@ -38,25 +38,24 @@ if [ ! -f /srv/dolibarr/documents/install.lock ]; then
 
     log 'Calling Dolibarr install (step 2)...'
     curl -kL -o /tmp/logs/install_step2.html -s -w "%{http_code}" \
-        -X POST "http://dolibarr:80/install/step2.php" \
+        -X POST "http://${DOLI_TEST_HOSTNAME}:80/install/step2.php" \
         --data "testpost=ok&action=set&dolibarr_main_db_character_set=utf8&dolibarr_main_db_collation=utf8_unicode_ci&selectlang=fr_FR" \
         > /tmp/logs/install_step2.status
 
     if ! grep -q 200 /tmp/logs/install_step2.status; then 
-        log 'Something went wrong during Dolibarr database installation. Check the logs for details.'
-        cat /tmp/logs/install_step2.html
+        log "Something went wrong during Dolibarr database installation. Check the logs for details: $(cat /tmp/logs/install_step2.html)"
         exit 1
     fi
 
     log 'Calling Dolibarr install (step 5)...'
     curl -kL -o /tmp/logs/install_step5.html -s -w "%{http_code}" \
-        -X POST "http://dolibarr:80/install/step5.php" \
-        --data "testpost=ok&action=set&selectlang=fr_FR&pass=${DOLIBARR_DB_PASSWORD}&pass_verif=${DOLIBARR_DB_PASSWORD}" \
+        -X POST "http://${DOLI_TEST_HOSTNAME}:80/install/step5.php" \
+        --data "testpost=ok&action=set&selectlang=fr_FR&pass=${DOLI_DB_PASSWORD}&pass_verif=${DOLI_DB_PASSWORD}" \
         > /tmp/logs/install_step5.status
 
+    cat /tmp/logs/install_step5.status
     if ! grep -q 200 /tmp/logs/install_step5.status; then 
-        log 'Something went wrong during Dolibarr administration setup. Check the logs for details.'
-        cat /tmp/logs/install_step5.html
+        log "Something went wrong during Dolibarr administration setup. Check the logs for details: $(cat /tmp/logs/install_step5.html)"
         exit 1
     fi
 
